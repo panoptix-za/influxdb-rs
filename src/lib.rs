2c846498ffb5cfa6470b3e490c7afd04a9395eda
Jake Goulding <jake.goulding@gmail.com>
2017-02-10 15:03:45 -0500

    Add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,74 @@
+//! # influxdb
+//!
+//! influxdb provides an asynchronous Rust interface to an
+//! [InfluxDB][] database.
+//!
+//! This crate supports insertion of strings already in the InfluxDB
+//! Line Protocol. The `influxdb-derive` crate provides convenient
+//! serialization of Rust structs to this format.
+//!
+//! [InfluxDB]: https://www.influxdata.com/
+//!
+//! # Examples
+//!
+//! To serialize a struct into the InfluxDB Line Protocol format, use the
+//! `influxdb-derive` crate's macros as shown below with `MyMeasure`.
+//!
+//! Then create an instance of `influxdb::AsyncDb` and add instances of
+//! your struct. Check out the code in the `examples` directory to see how
+//! this code interacts with futures.
+//!
+//! ```
+//! extern crate influxdb;
+//! #[macro_use]
+//! extern crate influxdb_derive;
+//! extern crate tokio_core;
+//!
+//! use std::time::SystemTime;
+//! use influxdb::{Measurement, AsyncDb};
+//!
+//! // `Measurement` is the trait that `AsyncDb` needs in order to insert
+//! #[derive(Measurement)]
+//! // The default measurement name will be the struct name; this optional
+//! // annotation allows customization of the name sent to InfluxDB.
+//! #[influx(rename = "my_measure")]
+//! struct MyMeasure {
+//!     // Specify which struct fields are InfluxDB tags.
+//!     // Tags must be `String`s or `&str`s.
+//!     #[influx(tag)]
+//!     region: String,
+//!     // Specify which struct fields are InfluxDB fields.
+//!     // Supported types are integers, floats, strings, and booleans.
+//!     // The rename annotation works with struct fields as well.
+//!     #[influx(field, rename = "amount")]
+//!     count: i32,
+//!     // Specify which struct field is the InfluxDB timestamp.
+//!     #[influx(timestamp)]
+//!     when: SystemTime,
+//!     // Struct fields that aren't annotated won't be sent to InfluxDB.
+//!     other: i32,
+//! }
+//!
+//! fn main() {
+//!     let mut core = tokio_core::reactor::Core::new()
+//!         .expect("Unable to create reactor core");
+//!
+//!     let async_db = AsyncDb::new(
+//!         core.handle(),            // A tokio_core handle
+//!         "http://localhost:8086/", // URL to InfluxDB
+//!         "my_database"             // Name of the database in InfluxDB
+//!     ).expect("Unable to create AsyncDb");
+//!
+//!     let now = SystemTime::now();
+//!     let batch = vec![
+//!         MyMeasure { region: String::from("us-east"), count: 3, when: now, other: 0 },
+//!         MyMeasure { region: String::from("us-west"), count: 20, when: now, other: 1 },
+//!     ];
+//!
+//!     let insert = async_db.add_data(&batch); // Returns a Future
+//! }
+//! ```
+
 extern crate tokio_core;
 extern crate hyper;
 extern crate futures;
